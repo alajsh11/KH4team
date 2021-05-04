45e8c550f313baf18ccd35f872be4a62f2887da1
Administrator <Administrator@DESKTOP-V5K10DS>
2021-05-04 11:23:22 +0900

    asdsa

--- src/run/Run.java
+++ src/run/Run.java
@@ -2,11 +2,8 @@ package run;
 
 public class Run {
 
-	public static void main(String[] args) {
-		
-		System.out.println("Hello This is Github Practicing!");
-		
-		System.out.println("hi");
-	}
 
+	public static void main(String[] args) {
+		System.out.println("Hi");
+	}
 }
